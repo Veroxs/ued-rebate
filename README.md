ddcc24983b0195274e772a1849c50b7dc4727b81
Verhk <631583063@qq.com>
2017-02-04 18:15:44 +0800

    ued-rebate

---
+++ README.md
@@ -0,0 +1,32 @@
+####目录结构
+
+	/ 根目录
+	|__ src 开发目录
+	|   |__ fonts 字体文件
+	|   |__ img 图片目录
+	|   |__ less 样式目录
+	|   |   |__ app 业务代码
+	|   |   |__ core 通用可移植的代码
+	|   |   |__ lib 第三方基本库
+	|   |   |__ *.less 各个模块统一入口
+	|   |__ js 目录
+	|   |   |__ app 业务代码
+	|   |   |__ core 通用可移植的代码
+	|   |   |__ lib 第三方基本库
+	|__ gulpfile.js gulp配置文件
+	|__ package.json npm依赖管理
+
+##### 说明：
+
+现在css的预编译语言比较多种，后面逐步转换成less + postcss。
+
+
+#### 关于编译
+
+ued-lib作为基本的ued源码，执行编译之后会输出到jia-mobile-web目录，请确保jia-mobile-web和ued-lib在同一个目录下面。
+
+
+#### 关于与jia-mobile-web的标示
+
+在每次改完jia-mobile-web的issue之后打上jia-mobile-web的jira标记。
+
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require("gulp"),
+    less = require("gulp-less"),
+    postcss = require('gulp-postcss'),
+    cssurl = require('gulp-cssurl'),
+    base64 = require('gulp-base64'),
+    processors = [
+        require('autoprefixer'),
+        require('css-mqpacker'),
+        require('postcss-pxtorem')({
+            //rootValue: 33.75 * 3,// 1080/3 1080的设计稿 flexible.js
+            rootValue: 3.375 * 100,// 1080/3 1080的设计稿 autoFize.js
+            unitPrecision: 5,
+            propWhiteList: [],
+            selectorBlackList: [],
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 0
+        }),
+        require('cssnano')
+    ],
+    path = {
+        dist: 'dist/',
+        dev: 'dev/'
+    }
+
+
+/*返利*/
+gulp.task('indexLess', function () {
+    gulp.src(path.dev + "/less/app/*.less")
+        .pipe(less())
+        .pipe(postcss(processors))
+        .pipe(cssurl())
+        .pipe(gulp.dest(path.dist + "/css/"))
+        .pipe(base64({maxImageSize: 20 * 1024}))
+
+    gulp.src(path.dev + "/img/**")
+        .pipe(gulp.dest(path.dist + "/images/"))
+
+    gulp.src(path.dev + "/less/img/**")
+        .pipe(gulp.dest(path.dist + "/css/img/"))
+
+    gulp.src(path.dev + "/js/**")
+        .pipe(gulp.dest(path.dist + "/js/"))
+})
+gulp.task('base', function () {
+    gulp.src(path.dev + "/less/base.less")
+        .pipe(less())
+        .pipe(postcss(processors))
+        .pipe(cssurl())
+        .pipe(gulp.dest(path.dist + "/css/"))
+        .pipe(base64({maxImageSize: 20 * 1024}))
+})
+
+gulp.task('default', ['indexLess', 'base'], function () {
+    gulp.watch(path.dev + "/less/**/*.less", ['indexLess', 'base'])
+});
